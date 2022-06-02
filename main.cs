6c5cc61485156cf3c093a78dba1102e45b2017de
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-06-02 16:40:22 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,29 @@
+// Created by: Johanna Liu
+// Created on: April 2022
+//
+// This program accepts user input
+
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        HttpClient client = new HttpClient();
+        string response = await client.GetStringAsync(
+            "https://api.thecatapi.com/v1/images/search"
+        );
+        var json = System.Text.Json.JsonSerializer.Deserialize<Object>(response);
+        JsonNode jsonData = JsonNode.Parse(response)!;
+        JsonNode url = jsonData[0]["url"];
+
+        Console.WriteLine("Random Cat picture " + url + " .");
+        Console.WriteLine("\nDone");
+    }
+}
